5b61a131533d0e030056dfff82fa5e1d33e3900a
Gniewo1 <136825015+Gniewo1@users.noreply.github.com>
2025-01-24 19:43:36 +0100

    COŚ

--- backend/users/signals.py
+++ backend/users/signals.py
@@ -12,8 +12,8 @@ def create_verification_code_and_send_email(sender, instance, created, **kwargs)
         
         # Send email with the verification code
         send_mail(
-            subject="Your Verification Code",
-            message=f"Dear {instance.username},\n\nYour verification code is: {verification_code.code}\n\nThank you for registering!",
+            subject="Kod Weryfikacyjny",
+            message=f"Dear {instance.username},\n\nTwój kod weryfikacyjny to: {verification_code.code}\n\nDziękuję za rejestrację!",
             from_email=settings.DEFAULT_FROM_EMAIL,
             recipient_list=[instance.email],
             fail_silently=False,
