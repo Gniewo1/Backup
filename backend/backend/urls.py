b3a7e36776157e22a629871c5d97032f3b08d8ac
Gniewo1 <136825015+Gniewo1@users.noreply.github.com>
2025-03-19 17:21:33 +0100

    nic

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('8zIePhw7tCSwlGZ/', admin.site.urls),
     path('api/', include('users.urls')),
     path('cards/', include('cards.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
